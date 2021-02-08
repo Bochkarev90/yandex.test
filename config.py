3e2a0f4ac095a23f82a1726eb21be93dd0aef6b4
max <maxim.bochkarev@perfectart.com>
2021-02-08 20:29:27 +0300

    Added headless

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 timeout = 4
 treads = 2
+headless = True
--- conftest.py
+++ conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from selenium import webdriver
 
+from config import headless
+
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    if bool(headless):
+        options.add_argument('headless')
+    driver = webdriver.Chrome(options=options)
     driver.get('https://yandex.ru')
     yield driver
     driver.quit()
